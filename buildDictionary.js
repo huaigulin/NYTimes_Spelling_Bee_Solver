9e8350d3cd54eaa146ae1e43d87759c424665c5b
Huaigu Lin <kevinlinhg@outlook.com>
2020-05-04 16:26:28 +0800

    add a word list

--- buildDictionary.js
+++ buildDictionary.js
@@ -5,13 +5,16 @@ const fs = require("fs"),
 const insertWords = async () => {
   const db = await getDb();
   const readInterface = readline.createInterface({
-    input: fs.createReadStream("./corncob_lowercase.txt"),
+    input: fs.createReadStream("./words_alpha.txt"),
     output: process.stdout,
     console: false,
   });
 
   readInterface.on("line", (word) => {
-    db.collection("dictionary").insertOne({ word: word });
+    db.collection("dictionary").insertOne({ word: word }, (error, result) => {
+      if (error) throw error;
+      console.log(`Inserted word: ${word}`)
+    });
   });
   db.collection("dictionary").createIndex({ word: 1 });
 };
